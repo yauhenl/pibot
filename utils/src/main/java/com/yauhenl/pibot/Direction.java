55d77bc362dcb18187469ea22c1a7e813c61b8b5
yauhenl <yauhen.lisimau@gmail.com>
2016-07-22 20:51:54 +0300

    update motors numbers

--- utils/src/main/java/com/yauhenl/pibot/Direction.java
+++ utils/src/main/java/com/yauhenl/pibot/Direction.java
@@ -2,13 +2,13 @@ package com.yauhenl.pibot;
 
 /*
     dc1 32, 16
-    dc2 8, 64
+    dc2 64, 8
     dc3 1, 4
-    dc4 128, 2
-*/
+    dc4 2, 128
+ */
 public enum Direction {
-    forward(169), //32+8+1+128
-    backward(86), //16+64+4+2
+    forward(99), //32+64+1+2
+    backward(156), //16+8+4+128
     left(46),     //32+8+4+2
     right(209),   //16+64+1+128
     stop(0);
