016935fc5fbeb317554294f3df6381674d1cc2bd
yauhenl <yauhen.lisimau@gmail.com>
2016-09-23 19:01:36 +0300

    remove speed control

--- simplebot/src/main/java/com/yauhenl/pibot/Application.java
+++ simplebot/src/main/java/com/yauhenl/pibot/Application.java
@@ -1,6 +1,5 @@
 package com.yauhenl.pibot;
 
-import com.sun.net.httpserver.HttpServer;
 import com.yauhenl.pibot.web.BotController;
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -11,9 +10,9 @@ import java.net.URI;
 
 public class Application {
     public static void main(String[] args) throws IOException {
-        URI baseUri = UriBuilder.fromUri("http://localhost/").port(9998).build();
+        URI baseUri = UriBuilder.fromUri("http://localhost/").port(8080).build();
         ResourceConfig config = new ResourceConfig(BotController.class);
-        HttpServer server = JdkHttpServerFactory.createHttpServer(baseUri, config);
+        JdkHttpServerFactory.createHttpServer(baseUri, config);
     }
 }
 
