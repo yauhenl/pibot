cac476e39cfa6a76a7fdc93b00520a942d81419d
yauhenl <yauhen.lisimau@gmail.com>
2016-11-28 22:48:23 +0300

    fix bot

--- bot/src/main/kotlin/com/yauhenl/pibot/web/IndexController.kt
+++ bot/src/main/kotlin/com/yauhenl/pibot/web/IndexController.kt
@@ -5,7 +5,7 @@ import com.yauhenl.pibot.service.ActionService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
 class IndexController {
@@ -14,5 +14,5 @@ class IndexController {
     private val actionService: ActionService? = null
 
     @GetMapping("/action/{actionName}")
-    fun performAction(@RequestParam("actionName") actionName: String) = actionService?.performAction(ActionType.valueOf(actionName))
+    fun performAction(@PathVariable("actionName") actionName: String) = actionService?.performAction(ActionType.valueOf(actionName))
 }
